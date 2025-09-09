c3c8d84c2c638d9e9e84f5927a352ef129f5ebb3
ゆーか <yuka.pooh3028@gmail.com>
2025-09-09 10:30:52 +0900

    ruby_setup

---
+++ ruby/main.rb
@@ -0,0 +1,7 @@
+require 'tf-idf-similarity'
+require 'annoy'
+
+puts "Hello from Ruby!"
+puts "Ruby version: #{RUBY_VERSION}"
+puts "TF-IDF Similarity gem loaded successfully!"
+puts "Annoy gem loaded successfully!"
